475bc5a7f967887f2a31b85fe22bd0a113dab4fb
iamabdullah07 <darkjutt7777@gmail.com>
2024-12-02 19:01:09 +0500

    Commit
    YT colne

---
+++ YouTube.js
@@ -0,0 +1,8 @@
+function buttonsHandler(evt)
+{
+    if(evt.textContent == "#navbarNav")
+    {alert("Button1 Clicked");}
+
+    else if(evt.textContent == "Button2")
+    {alert("Button2 Clicked");}
+}
